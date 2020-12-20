d4706a7136622a05954529993712bd7621e6cce5
Sodiq_dev <57923180+Sodiq-123@users.noreply.github.com>
2020-12-20 13:27:52 +0100

    Update smtp_email.py

--- smtp_email.py
+++ smtp_email.py
@@ -1,1 +1,16 @@
+def send_email(subject, message, recipient, mail_type="html"):
+  context = ssl.create_default_context()
+  with smtplib.SMTP_SSL(app.config["MAIL_SETTINGS"]["HOST"], app.config["MAIL_SETTINGS"]["PORT"], context=context) as server: 
+        server.login(app.config["MAIL_SETTINGS"]["SENDER"], app.config["MAIL_SETTINGS"]['PASSWORD'])
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = app.config['MAIL_SETTINGS']['SENDER']
+        msg['To'] = recipient 
 
+        
+        if mail_type == 'html':
+            msg.attach(MIMEText(message, 'html'))
+        else:
+            msg.attach(MIMEText(message, 'plain'))
+
+        server.sendmail(app.config['MAIL_SETTINGS']['SENDER'], recipient, msg.as_string())
